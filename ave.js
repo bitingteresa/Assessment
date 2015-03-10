772f81ee88e95f36118a8458d7a459dd506ed83b
bitingteresa <teresa@scientificlee.com>
2015-03-10 18:33:29 -0400

    An actual loop

--- ave.js
+++ ave.js
@@ -1,60 +1,14 @@
 var sget = require('sget');
 var userTempArray =[];
+var userInput;
 
-
-function run(){
-	tempOne();
-	tempTwo();
-	tempThree();
-	tempFour();
-	tempFive();
-	tempSix();
-	tempSeven();
-	average();
+while (userTempArray.length < 7) {
+	enterTemp();
 }
 
-function tempOne () {
-	var userTempOne = parseInt(sget('Please enter in first temperature:').trim());
-	userTempArray.push(userTempOne);
-}
+function enterTemp() {
+		userInput = parseInt(sget('Please enter in temperature:').trim());
+		userTempArray.push(userInput);
+	}
 
-function tempTwo () {
-	var userTempTwo = parseInt(sget('Please enter in second temperature:').trim());
-	userTempArray.push(userTempTwo);
-}
-
-function tempThree () {
-	var userTempThree = parseInt(sget('Please enter in third temperature:').trim());
-	userTempArray.push(userTempThree);
-}
-
-function tempFour () {
-	var userTempFour = parseInt(sget('Please enter in fourth temperature:').trim());
-	userTempArray.push(userTempFour);
-}
-
-function tempFive () {
-	var userTempFive = parseInt(sget('Please enter in fifth temperature:').trim());
-	userTempArray.push(userTempFive);
-}
-
-function tempSix () {
-	var userTempSix = parseInt(sget('Please enter in sixth temperature:').trim());
-	userTempArray.push(userTempSix);
-}
-
-function tempSeven () {
-	var userTempSeven = parseInt(sget('Please enter in seventh temperature:').trim());
-	userTempArray.push(userTempSeven);
-}
-
-var Sum = 0
-function average() {
-for(var i = 0; i < userTempArray.length; i++) {
-	Sum = Sum + userTempArray[i]
-	var average = Sum / userTempArray[i]
-}
-}
-
-run();
-console.log(average());
+console.log(userTempArray);
