682f0be8c584e3c0b10b58201e62b21afc50c1a2
bitingteresa <teresa@scientificlee.com>
2015-03-11 16:17:56 -0400

    Added verifyInput

--- ave.js
+++ ave.js
@@ -8,14 +8,23 @@ while (userTempArray.length < 7) {
 
 function enterTemp() {
 		userInput = parseInt(sget('Please enter in temperature:').trim());
+		verifyInput(userInput);
 		userTempArray.push(userInput);
 	}
 
 function average() {
 	var sum = userTempArray.reduce(function(temp1, temp2) {
 	return temp1 + temp2; });
-	return sum/userTempArray.length;
+	return Math.round(sum/userTempArray.length);
 }
 
+function verifyInput() {
+	if (isNaN(userInput)) {
+		console.log("Please enter a valid entry.")
+		enterTemp();
+	} else {
+		return true;
+	}
+}
 console.log(userTempArray);
 console.log(average());
